5be8e93d7e406d24383d18bcf3ac0aee2b62dda5
Tushar Gaur <tushar.gaur@procdna.com>
2026-09-09 16:40:55 +0530

    fix: rebase issue

--- src/config/Apiconfig.js
+++ src/config/Apiconfig.js
@@ -1,5 +1,5 @@
 // base url for the API
-export const API_BASE_URL = 'https://hwtkv02r-8000.inc1.devtunnels.ms';
+export const API_BASE_URL = 'https://hwtkv02r-8100.inc1.devtunnels.ms';
 
 // test workflow id 
 export const WORKFLOW_ID = '0851e73e-42ff-4eb3-b967-e21e6dd26520';
